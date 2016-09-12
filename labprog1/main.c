ba925683c9693af2a284ae4e62629f3c6573253e
sarthak98 <saka16ec@cmrit.ac.in>
2016-09-12 20:09:14 +0530

    Update main.c

--- labprog1/main.c
+++ labprog1/main.c
@@ -21,7 +21,7 @@
     {
     rr=(-b/(2*a));
     ir=(sqrt(-d))/(2*a);
-    printf("roots are imaginary r1=%f+i%f and r2=%f+i%f",rr,ir,rr,ir);
+    printf("roots are imaginary r1=%f+i%f and r2=%f-i%f",rr,ir,rr,ir);
     }
 
 return 0;
